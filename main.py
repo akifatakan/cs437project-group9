05d25c273d6760d3b475513867eec13ab231d94a
begumolcayto <103627120+begumolcayto@users.noreply.github.com>
2023-12-29 22:06:12 +0300

    login page initialized

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def index():
 def signup_page():
     return render_template('signup.html')
 
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
