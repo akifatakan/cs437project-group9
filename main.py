66b02cdb940b5804a5861656cfebfb1d9c7b573c
Akif Atakan Yılmaz <akifatakan@sabanciuniv.edu>
2024-01-01 22:24:29 +0300

    signup page developed

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+class SignupForm(FlaskForm):
+    username = StringField("Username:",)
+    email = StringField("Email:")
+    password = StringField("Password:")
+
+    submit = SubmitField("Submit")
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,9 +21,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup_page():
-    return render_template('signup.html')
+
+    signupForm = SignupForm()
+
+
+    if signupForm.validate_on_submit():
+        session["username"] = signupForm.username.data
+        session["email"] = signupForm.email.data
+        session["password"] = signupForm.password.data
+
+        return redirect(url_for("index"))
+
+    return render_template('signup.html', signupForm=signupForm)
 
 @app.route('/login')
 def login_page():
